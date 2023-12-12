28f9ea6624c28326e0e2b2bb5e7d912cef547847
unknown <umandaravimal@gmail.com>
2023-12-12 22:50:18 +0530

    Build calculator visual

--- src/App.js
+++ src/App.js
@@ -4,22 +4,33 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='display'>0</div>
+      <CalcButton value="7"/>
+      <CalcButton value="8"/>
+      <CalcButton value="9"/>
+      <CalcButton className="operator" value="/"/>
+
+      <CalcButton value="4"/>
+      <CalcButton value="5"/>
+      <CalcButton value="6"/>
+      <CalcButton className="operator" value="*"/>
+
+      <CalcButton value="3"/>
+      <CalcButton value="2"/>
+      <CalcButton value="1"/>
+      <CalcButton className="operator" value="-"/>
+
+      <CalcButton value="C"/>
+      <CalcButton value="0"/>
+      <CalcButton value="="/>
+      <CalcButton className="operator" value="+"/>
     </div>
   );
+}
+
+function CalcButton(props){
+  return <button className={props.className}>{props.value}</button>
+
 }
 
 export default App;
